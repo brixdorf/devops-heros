0427108d678238f0175396ebcc523623de858573
brixdorf <itsromit@protonmail.com>
2026-09-03 22:59:55 +0530

    docker fundamentals md done

---
+++ Session-6 (Docker Fundamentals)/DOCKER_FUNDAMENTALS.md
@@ -0,0 +1,18 @@
+# Docker Homework: Hello World Applications
+
+## Six Hello World apps, each in its own folder with a Dockerfile, built and run separately.
+
+- **nginx-app** - static HTML served by Nginx
+- **python-app** - Python built-in HTTP server
+- **nodejs-app** - Node built-in HTTP server
+- **java-app** - Java built-in HttpServer
+- **apache-app** - static HTML served by Apache httpd
+- **react-app** - React app, multi-stage build, served by Nginx
+
+## Screenshots
+![](image1.png)
+![](image2.png)
+![](image3.png)
+![](image4.png)
+![](image5.png)
+![](image6.png)
---
+++ Session-6 (Docker Fundamentals)/java-app/Main.java
@@ -0,0 +1,17 @@
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+import java.io.OutputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", exchange -> {
+            String response = "<h1>Hello World from Java</h1>";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.start();
+    }
+}
